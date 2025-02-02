f83a31d02fc2d6fba2653cab3a70db164e81a3f2
unknown <sanjeetkumarit@outlook.com>
2025-02-02 14:17:37 +0530

    beforeall playwright

--- e2e/tests/app.test.ts
+++ e2e/tests/app.test.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeAll(async () => {
+  console.log('Ensure Angular app is running on http://localhost:4200');
+});
+
 test('should load the app', async ({ page }) => {
-   await page.goto('/'); // Navigate to the home page
+   await page.goto('/'); //This will resolve to 'http://localhost:4200/' on playwright config baseURL
 
   // Ensure the app title is present
   const title = await page.title();
