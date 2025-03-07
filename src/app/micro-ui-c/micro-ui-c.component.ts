6b2278a7e9373f860c07dc503824613942be5916
unknown <sanjeetkumarit@outlook.com>
2025-03-07 14:24:31 +0530

    NOTE for proper web compoennt

--- src/app/micro-ui-c/micro-ui-c.component.ts
+++ src/app/micro-ui-c/micro-ui-c.component.ts
@@ -1,6 +1,12 @@
 import { Component,Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+//Please NOTE:  its just a child component inside the Parent UI.
+//A proper web-component  runs outside Angular’s change detection and outside  Angular’s lifecycle
+//A proper web-component -You cannot directly access it using 
+// @ViewChildren to check .isDirty() ,because Angular doesn’t recognize it as an Angular component.
+//In that case, two options for proper web-component:
+//✅ Option 1: Use window.addEventListener to Detect Changes,it should dispatch a custom event when data changes.
+//✅ Option 2: Listen for Events in ParentUiComponent
 
 @Component({
   selector: 'app-micro-ui-c',
--- src/app/micro-ui/micro-ui.component.ts
+++ src/app/micro-ui/micro-ui.component.ts
@@ -5,6 +5,14 @@ import { FormBuilder, FormGroup ,Validators } from '@angular/forms';
   templateUrl: './micro-ui.component.html',
   styleUrls: ['./micro-ui.component.css']
 })
+//Please NOTE:  its just a child component inside the Parent UI.
+//A proper web-component  runs outside Angular’s change detection and outside  Angular’s lifecycle
+//A proper web-component -You cannot directly access it using 
+// @ViewChildren to check .isDirty() ,because Angular doesn’t recognize it as an Angular component.
+//In that case, two options for proper web-component:
+//✅ Option 1: Use window.addEventListener to Detect Changes,it should dispatch a custom event when data changes.
+//✅ Option 2: Listen for Events in ParentUiComponent
+
 export class MicroUiComponent {
   @Input() form!: FormGroup;
   @Output() formDataChanged = new EventEmitter<any>(); // Emit data to parent
